12cb8e704eb8b475f105e8fd69434257a6deb559
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-04-17 11:42:04 +0600

    add filter options on the timeline components

--- src/components/pages/timelines/Timeline.jsx
+++ src/components/pages/timelines/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { TimelineContext } from '../../../context/TimelineProvider';
 import call from "/assets/call.png";
 import text from "/assets/text.png";
@@ -6,12 +6,19 @@ import video from "/assets/video.png";
 
 const Timeline = () => {
     const {timeline} = useContext(TimelineContext)
+    const [filter,setFilter] = useState('All')
+    const displayFilter = filter=='All'?timeline:timeline.filter(item=>item.type.toLowerCase()==filter.toLowerCase())
     return (
         <div className=' bg-gray-50 pb-12 pt-16'>
             <div className='max-w-[80%] mx-auto'>
                 <h1 className='text-3xl font-bold mb-6'>Timeline</h1>
-
-            {timeline.map(item=>(
+                <select onChange={(e)=>setFilter(e.target.value)} className='w-1/10 mb-6 outline-1 outline-gray-200'>
+                    <option>All</option>
+                    <option>Call</option>
+                    <option>Text</option>
+                    <option>Vedio</option>
+                </select>
+            {displayFilter.map(item=>(
                 <div className='flex items-center gap-2 p-3 rounded-lg bg-white shadow mb-4'>
                     <img className='w-5' src={`${item.type=='call'?call:item.type=='vedio'?video:text}`}></img>
                     <div>
